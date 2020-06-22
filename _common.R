d27965130697eb341401ab6a5bc17bc9ec045d20
Alfred Grant Schissler <grant.schissler@gmail.com>
2020-06-22 16:20:19 -0700

    customize output

---
+++ _common.R
@@ -0,0 +1,26 @@
+set.seed(12)
+options(digits = 4)
+
+knitr::opts_chunk$set(
+  # Text Results
+  eval = TRUE,
+  echo = FALSE,
+  results = 'markup',
+  collapse = TRUE,
+  warning = FALSE,
+  error = FALSE,
+  message = FALSE,
+  # Code Decoration
+  comment = NA,
+  background = '#F7F7F7',
+  # Cache
+  cache = TRUE,
+  # Plots
+  fig.show = "hold",
+  fig.width = 6.5,
+  fig.asp = 0.618,  # 1 / phi
+  out.width = "85%",
+  fig.align = 'center'
+)
+
+# options(dplyr.print_min = 6, dplyr.print_max = 6)
